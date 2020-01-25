b95f0a3d1383f7ace93cd429a6a828a79fe866a2
SavLM <50849597+SavLM@users.noreply.github.com>
2020-01-24 20:19:01 -0500

    finished up tutorial

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+# this makes the post info more clear on the admin page
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','created_on')
+    list_filter = ("status",)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)} # slug automatically filled based on title
+
+admin.site.register(Post, PostAdmin)
+
+#admin.site.register(Post)
